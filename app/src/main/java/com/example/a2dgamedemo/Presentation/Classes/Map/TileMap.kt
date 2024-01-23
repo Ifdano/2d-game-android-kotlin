6ccdc68896176d1e23eb7ab9f542beecf2aa4a81
asif.m <asif.m@setclapp.com>
2024-01-23 18:10:09 +0400

    Map class

--- app/src/main/java/com/example/a2dgamedemo/Presentation/Classes/Map/TileMap.kt
+++ app/src/main/java/com/example/a2dgamedemo/Presentation/Classes/Map/TileMap.kt
@@ -4,7 +4,12 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.icu.number.IntegerWidth
+import com.example.a2dgamedemo.Presentation.Helpers.DisplayHelper
 import com.example.a2dgamedemo.Presentation.Views.ITileMap
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
 
 class TileMap(context: Context, mapSize: Int) : ITileMap {
     val MAP = "map.txt"
@@ -28,7 +33,7 @@ class TileMap(context: Context, mapSize: Int) : ITileMap {
     private var rowOffset = 0
     private var colOffset = 0
 
-    private val map: Array<Array<Int>>? = null
+    private var map: Array<Array<Int>>? = null
 
     private var tileSet: Bitmap? = null
     private var tiles: Array<Array<Tile>>? = null
@@ -36,6 +41,52 @@ class TileMap(context: Context, mapSize: Int) : ITileMap {
     init {
         this.context = context
         this.mapSize = mapSize
+    }
+
+    override fun setX(value : Int){
+        x = value
+
+        if(x < minX) x = minX
+        if(x > maxX) x = maxX
+
+        colOffset = -x/mapSize
+    }
+
+    override fun getX() = x
+
+    override fun setY(value : Int){
+        y = value
+
+        if(y < minY) y = minY
+        if(y > maxY) y = maxY
+
+        rowOffset = -y/mapSize
+    }
+
+    override fun getY() = y
+
+    override fun getRowTile(y : Int): Int = y/mapSize
+    override fun getColTile(x : Int): Int = x/mapSize
+
+    override fun getMapSize(): Int = mapSize
+
+    private fun getCurrentRowCol(row : Int, col : Int) = map?.get(row)?.get(col)
+
+    override fun isBlocked(row: Int, col: Int): Boolean {
+        val rc = getCurrentRowCol(row, col)
+
+        val r = rc!! / tiles?.get(0)?.size!!
+        val c = rc % tiles?.get(0)?.size!!
+
+        return tiles!!.get(r).get(c).blocked
+    }
+
+    override fun setHeight(value: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setWidth(value: Int) {
+        TODO("Not yet implemented")
     }
 
     override fun draw(canvas: Canvas, paint: Paint) {
@@ -53,7 +104,7 @@ class TileMap(context: Context, mapSize: Int) : ITileMap {
                 if(map?.get(row)?.get(col) == 0)
                     continue
 
-                val currentRowCol = map?.get(row)?.get(col)
+                val currentRowCol = getCurrentRowCol(row, col)
 
                 val r = currentRowCol!! / tiles?.get(0)?.size!!
                 val c = currentRowCol % tiles?.get(0)?.size!!
@@ -71,4 +122,38 @@ class TileMap(context: Context, mapSize: Int) : ITileMap {
     override fun update() {}
 
     override fun readMap() {}
+    override fun loadMap() {
+        try{
+            val inputStreamReader = InputStreamReader(context?.openFileInput(MAP))
+            val bufferedReader = BufferedReader(inputStreamReader)
+
+            minX = DisplayHelper.getDisplayWidth() - mapWidth*mapSize
+            minY = DisplayHelper.getDisplayHeight() - mapHeight*mapSize
+
+            numRowToDraw = DisplayHelper.getDisplayHeight()/mapSize + 2
+            numColToDraw = DisplayHelper.getDisplayWidth()/mapSize + 2
+
+            map = arrayOf(arrayOf(mapHeight, mapWidth))
+            val delimeters = "\\s+"
+
+            for(row in 0 until mapHeight){
+                val line = bufferedReader.readLine()
+                val tokens = line.split(delimeters)
+
+                for(col in 0 until mapWidth){
+                    map!![row][col] = tokens[col].toInt()
+                }
+            }
+        }catch (exp : Exception){
+            exp.printStackTrace()
+        }
+    }
+
+    override fun loadTile() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTileSet(): Bitmap {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/example/a2dgamedemo/Presentation/Views/ITileMap.kt
+++ app/src/main/java/com/example/a2dgamedemo/Presentation/Views/ITileMap.kt
@@ -1,12 +1,35 @@
 package com.example.a2dgamedemo.Presentation.Views
 
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
 
 interface ITileMap {
+    fun setX(value : Int)
+    fun getX() : Int
+
+    fun setY(value : Int)
+    fun getY() : Int
+
+    fun getRowTile(y : Int) : Int
+    fun getColTile(x : Int) : Int
+
+    fun getMapSize() : Int
+
+    fun isBlocked(row : Int, col : Int) : Boolean
+
+    fun setHeight(value : Int)
+    fun setWidth(value : Int)
+
+    fun readMap()
+    fun loadMap()
+
+    fun loadTile()
+
+    fun getTileSet() : Bitmap
+
     fun draw(canvas: Canvas, paint: Paint)
 
     fun update()
 
-    fun readMap()
 }
